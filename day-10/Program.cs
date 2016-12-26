4bc62caa84100a0f45f2db30c87e54a418bbfa6f
Matt Cosand <matt@cosand.net>
2016-12-25 23:03:50 -0800

    day 10 part 1

---
+++ day-10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_10
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int count = 40;
+      //count = 5;
+      string input = "1113222113";
+      //input = "1";
+
+      for (int i=0; i<count;i++)
+      {
+        StringBuilder sb = new StringBuilder();
+        int j = 0;
+        char c;
+        while (j < input.Length)
+        {
+          int k = j;
+          c = input[j++];
+          while (j < input.Length && input[j] == c) j++;
+          sb.AppendFormat("{0}{1}", j - k, c);
+        }
+        input = sb.ToString();
+        // Console.WriteLine(input);
+        Console.WriteLine(sb.Length);
+      }
+    }
+  }
+}
